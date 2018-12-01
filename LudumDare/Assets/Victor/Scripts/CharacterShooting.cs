be982047f4eb5fb8b46f518662ab44aecc857820
Ruyuik <callotv@hotmail.fr>
2018-12-01 10:12:40 +0100

    Add FX

---
+++ LudumDare/Assets/Victor/Scripts/CharacterShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShooting : MonoBehaviour {
+
+    public GameObject bullet;
+    public float shootingCooldown;
+    bool isShooting;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && !isShooting)
+        {
+            isShooting = true;
+            StartCoroutine(CoolingdDown());
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+
+    IEnumerator CoolingdDown()
+    {
+        yield return new WaitForSeconds(shootingCooldown);
+        isShooting = false;
+    }
+}
