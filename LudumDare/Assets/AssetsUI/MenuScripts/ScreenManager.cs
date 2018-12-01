2b529f7c066393467e617ddffe96b0938abafd90
Aldrefan <brousse.dylan@gmail.com>
2018-12-01 08:22:26 +0100

    MainMenu

---
+++ LudumDare/Assets/AssetsUI/MenuScripts/ScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NewRun()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void FullscreenToggle()
+    {
+        if (Screen.fullScreen == true)
+        {
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            Screen.fullScreen = true;
+        }
+    }
+}
