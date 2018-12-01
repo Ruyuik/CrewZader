9f7649816f281f9111bc2799fd7914e34b07316e
benjilap <benjilap2@yahoo.fr>
2018-12-01 22:06:16 +0100

    up enemy

---
+++ LudumDare/Assets/Benji/Scripts/BomberBulletBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberBulletBehavior : MonoBehaviour {
+
+    public GameObject fragedBullet;
+    public int bulletFragment;
+
+    public float bulletSpeed;
+
+    public int bulletInd;
+    float ymodif;
+
+    float startTimer;
+    public float travelTime;
+    bool startFragmentation;
+
+    public List<Vector3> fragmentDir = new List<Vector3>();
+
+    public void InitDirection()
+    {
+        if (bulletInd == 0)
+        {
+            ymodif = 0.05f;
+        }
+        else if (bulletInd == 1)
+        {
+            ymodif = 0;
+        }
+        else if (bulletInd == 2)
+        {
+            ymodif = -0.05f;
+        }
+        startTimer = Time.time;
+        SetFragmentDir();
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(-0.1f*bulletSpeed, ymodif);
+        if (startFragmentation == false)
+        {
+            BulletFragmentation();
+        }
+
+
+        if (transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void BulletFragmentation()
+    {
+        if (Time.time - startTimer >= travelTime)
+        {
+            startFragmentation = true;
+            for(int i =0; i < bulletFragment; i++)
+            {
+                GameObject myNewBullet = Instantiate(fragedBullet, transform.position + fragmentDir[i], Quaternion.identity);
+                myNewBullet.GetComponent<FraggedBomberBulletBehavior>().bulletDir = fragmentDir[i];
+            }
+            Destroy(gameObject);
+
+        }
+
+    }
+
+    void SetFragmentDir()
+    {
+        for (float i = 0; i <= 360; i += 360 / bulletFragment)
+        {
+            fragmentDir.Add(new Vector3(Mathf.Cos(i), Mathf.Sin(i), 0));
+        }
+    }
+}
---
+++ LudumDare/Assets/Benji/Scripts/EnnemisPatternBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemisPatternBehavior : MonoBehaviour {
+
+    public List<GameObject> enemy_Nav = new List<GameObject>();
+    public List<GameObject> enemy_NavFront = new List<GameObject>();
+    public List<GameObject> enemy_NavBack = new List<GameObject>();
+    public List<GameObject> enemy_NavSpawn = new List<GameObject>();
+
+    void Start () {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            enemy_Nav.Add(transform.GetChild(i).gameObject);
+
+        }
+        for (int i = 0; i < enemy_Nav.Count; i++)
+        {
+            if (transform.GetChild(i).position.x < 4)
+            {
+                enemy_NavFront.Add(transform.GetChild(i).gameObject);
+            }
+            if (transform.GetChild(i).position.x > 4)
+            {
+                enemy_NavBack.Add(transform.GetChild(i).gameObject);
+            }
+            if (transform.GetChild(i).position.x ==8 )
+            {
+                enemy_NavSpawn.Add(transform.GetChild(i).gameObject);
+            }
+        }
+
+    }
+	
+
+}
