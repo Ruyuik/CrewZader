e26eb04f0b5e226e06921400a3d53a352ef8e51a
Kaldrin <bastien69580@gmail.com>
2018-12-03 23:06:41 +0100

    EndGameScore

--- LudumDare/Assets/bastFiles/Scripts/GetScore.cs
+++ LudumDare/Assets/bastFiles/Scripts/GetScore.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class GetScore : MonoBehaviour
 {
 	public static int score;
+	private ScoreScript scoreScript;
 	
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		scoreScript = FindObjectOfType<ScoreScript>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		score = scoreScript.playerScore;
+
+			
+	}
+
+	public int GetStaticScore()
+	{
+		return score;
+	}
+
+	public void setScoreTo0()
+	{
+		score = 0;
 	}
 }
