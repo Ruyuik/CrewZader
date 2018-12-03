10e8f0179e4d9eeb1abc30dc46a6ce720036cf6d
Ruyuik <callotv@hotmail.fr>
2018-12-03 17:46:56 +0100

    Capacities Sliders

---
+++ LudumDare/Assets/Victor/Scripts/BoostSliderBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoostSliderBehavior : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GetComponent<Slider>().value = 1;
+    }
+
+    void Update()
+    {
+        if (GetComponent<Slider>().value <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
